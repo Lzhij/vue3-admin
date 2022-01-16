0a949567b36f90105890000986a5ab849395e067
lzj <563086368@qq.com>
2022-01-16 11:22:38 +0800

    add 全局loading

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import { useStore } from '@/store'
+import { message } from 'ant-design-vue'
+
+let hide = null
 
 const serve = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_URL,
@@ -8,6 +11,7 @@ const serve = axios.create({
 
 serve.interceptors.request.use(
   (config) => {
+    !hide && (hide = message.loading('加载中...', 0))
     const { state } = useStore()
     if (state.token) {
       config.headers.Authorization = `Bearer ${state.token}`
@@ -20,9 +24,11 @@ serve.interceptors.request.use(
 serve.interceptors.response.use(
   (data) => {
     const result = data && data.data && data.data.data
+    hide() && (hide = null)
     return result
   },
   (err) => {
+    hide() && (hide = null)
     return Promise.reject(err)
   }
 )
